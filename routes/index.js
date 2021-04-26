df70a132d8095272aba02824330f5ce2709dc4be
tomassalem <tomassalem9@gmail.com>
2021-04-25 23:11:45 -0300

    ultimos del team, terminado 10 puntos

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const mainController = require('../controllers/mainController');
-const { route } = require('./product');
 
 /* GET home page. */
 router.get('/', mainController.index);
