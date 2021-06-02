9165cb35d8bff6df2dcd5d63038b07cbfd09aec8
tomassalem <tomassalem9@gmail.com>
2021-06-02 15:09:24 -0300

    cambios

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -2,13 +2,13 @@ let productosComputacion = require('../data/data');
 const db = require('../database/models');
 // const op = db.Sequelize.Op
 let mainController = {
-    index: function (req, res){
-        return res.render('index', {title: 'Tec', productosCompu: productosComputacion})
-    },
+    // index: function (req, res){
+    //     return res.render('index', {title: 'Tec', productosCompu: productosComputacion})
+    // },
     index: function(req, res){
         db.Product.findAll()
             .then( data => {
-                return res.render('index', { productosCompu: data , title: 'Tec'})
+                return res.render('index', {title: 'Tec', productosCompu: data})
             })
             .catch(error =>{
                 console.log(error);
--- database/models/Product.js
+++ database/models/Product.js
@@ -34,7 +34,7 @@ module.exports = function(sequelize, dataTypes){
     let config = {
         tableName: 'tablaproductos', 
         timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
-        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
+        underscored: false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
     }
 
    const Product = sequelize.define(alias, cols, config);
